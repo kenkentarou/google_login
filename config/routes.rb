5f8fd85981cea57f975d0d108d18c013ca059566
Taro <zettaigoukaku547@gmail.com>
2020-02-11 23:18:37 +0900

    コミット

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+      omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  root to: "static_pages#home"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get 'static_pages/home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
